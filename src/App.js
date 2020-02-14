49050d15495f51d02d73ee92208770a6662f0f7f
SRBond <skyler.bond7@gmail.com>
2020-02-14 11:01:01 -0800

    Add dependencies and implement react-bootstrap in app

--- src/App.js
+++ src/App.js
@@ -11,45 +11,47 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
-function App() {
-	return (
-		<div className="App">
-			<nav>
-				<Container>
-					<Row>
-						<Col>
-							<Button className="btn btn-primary">
-								<Link className="homeLink" to="/">
-									Home
-								</Link>
-							</Button>
-						</Col>
-						<Col>
-							<Button className="btn btn-secondary">
-								<Link className="createLink" to="/create">
-									Create
-								</Link>
-							</Button>
-						</Col>
-						<Col>
-							<Button className="btn btn-secondary">
-								<Link className="aboutLink" to="/about">
-									About
-								</Link>
-							</Button>
-						</Col>
-					</Row>
-				</Container>
-			</nav>
-			<main>
-				<Switch>
-					<Route path="/create" render={() => <Create />} />
-					<Route path="/about" render={() => <About />} />
-					<Route path="/" render={() => <Home />} />
-				</Switch>
-			</main>
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<nav>
+					<Container>
+						<Row>
+							<Col>
+								<Button className="btn btn-primary">
+									<Link className="homeLink" to="/">
+										Home
+									</Link>
+								</Button>
+							</Col>
+							<Col>
+								<Button className="btn btn-secondary">
+									<Link className="createLink" to="/create">
+										Create
+									</Link>
+								</Button>
+							</Col>
+							<Col>
+								<Button className="btn btn-secondary">
+									<Link className="aboutLink" to="/about">
+										About
+									</Link>
+								</Button>
+							</Col>
+						</Row>
+					</Container>
+				</nav>
+				<main>
+					<Switch>
+						<Route path="/create" render={() => <Create />} />
+						<Route path="/about" render={() => <About />} />
+						<Route path="/" render={() => <Home />} />
+					</Switch>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default App;
