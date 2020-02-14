0d64f16e10e5084cc7aa194fba12feaec37aac00
SRBond <skyler.bond7@gmail.com>
2020-02-14 12:45:40 -0800

    Added conditional buttons to portfolios in Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-// import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 // import Col from "react-bootstrap/Col";
 // import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 function Home(props) {
-	console.log(props);
 	const { ports, user } = props;
-	let numPorts = 0;
 	let portElems = ports.map(port => {
+		let comments = port.posts.map(comment => {
+			return <li>{comment}</li>;
+		});
 		return (
 			<div key={port._id}>
 				<img src={port.imageUrl} />
@@ -17,12 +18,19 @@ function Home(props) {
 				<h4>{port.name}</h4>
 				<h3>{port.title}</h3>
 				<p>{port.description}</p>
+				<ul>{comments}</ul>
 				<a href={port.link}>Visit portfolio</a>
+				{user._id === port.userId && (
+					<div>
+						<Button>Edit</Button>
+						<Button>Delete</Button>
+					</div>
+				)}
 			</div>
 		);
 	});
-	if (ports[0]) {
-		return <Container>{portElems}</Container>;
+	if (portElems) {
+		return <Container className="Home">{portElems}</Container>;
 	} else {
 		return <h1>No portfolios found?</h1>;
 	}
