fc61859e08418b62da3dbf72d2e0bf24e55ec719
SRBond <skyler.bond7@gmail.com>
2020-02-14 12:08:34 -0800

    Basic about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function About() {
-	return <div className="About">About page</div>;
+	return (
+		<div className="About">
+			<p>
+				This application is named Portfolio Rater. It is a way for users to
+				receive feedback on their professional portfolios. You may post your
+				portfolio to be listed on the website, where other users can comment on
+				the portfolio and provide feedback.
+			</p>
+		</div>
+	);
 }
 
 export default About;
