f83e6a3a4b3ffb1129edab0bd996f2e57aafbffb
SRBond <skyler.bond7@gmail.com>
2020-02-14 10:45:54 -0800

    Implement react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<nav>
+				<Container>
+					<Row>
+						<Col>
+							<Button className="btn btn-primary">
+								<Link className="homeLink" to="/">
+									Home
+								</Link>
+							</Button>
+						</Col>
+						<Col>
+							<Button className="btn btn-secondary">
+								<Link className="createLink" to="/create">
+									Create
+								</Link>
+							</Button>
+						</Col>
+						<Col>
+							<Button className="btn btn-secondary">
+								<Link className="aboutLink" to="/about">
+									About
+								</Link>
+							</Button>
+						</Col>
+					</Row>
+				</Container>
+			</nav>
+			<main>
+				<Switch>
+					<Route path="/create" render={() => <Create />} />
+					<Route path="/about" render={() => <About />} />
+					<Route path="/" render={() => <Home />} />
+				</Switch>
+			</main>
+		</div>
+	);
 }
 
 export default App;
