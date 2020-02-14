1c963482d0480fd9d9f019c7adf3fe14b04e4285
SRBond <skyler.bond7@gmail.com>
2020-02-14 10:38:22 -0800

    Add file structure and tests

--- src/components/Portfolio/Portfolio.test.js
+++ src/components/Portfolio/Portfolio.test.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import { create } from "react-test-renderer";
 
 describe("things should happen", () => {
 	test("thing happens", () => {});
