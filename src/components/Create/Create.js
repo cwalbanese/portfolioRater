dcb5a37d6af30063907a7c132cc629e57f99cf6a
cwalbanese <631gti@gmail.com>
2020-02-18 14:35:55 -0500

    Updated button
    Updated button color on create page

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,68 +1,70 @@
-import React from "react";
+import React from 'react';
 
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function Create() {
-	function postNewPortfolio(e) {
-		e.preventDefault();
-		const { name, imageUrl, title, link, description } = e.target;
-		let data = {
-			name: name.value,
-			imageUrl: imageUrl.value,
-			title: title.value,
-			link: link.value,
-			description: description.value
-		};
-		fetch("https://portfolio-rater.herokuapp.com/api/portfolios/", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			mode: "cors",
-			body: JSON.stringify(data)
-		}).then(function(response) {
-			console.log(response.json());
-		});
-	}
+  function postNewPortfolio(e) {
+    e.preventDefault();
+    const { name, imageUrl, title, link, description } = e.target;
+    let data = {
+      name: name.value,
+      imageUrl: imageUrl.value,
+      title: title.value,
+      link: link.value,
+      description: description.value
+    };
+    fetch('https://portfolio-rater.herokuapp.com/api/portfolios/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: 'cors',
+      body: JSON.stringify(data)
+    }).then(function(response) {
+      console.log(response.json());
+    });
+  }
 
-	return (
-		<div className="Create">
-			<Container fluid="true">
-				<Form onSubmit={postNewPortfolio}>
-					<Form.Row>
-						<Form.Group>
-							<Form.Label htmlFor="name">User name: </Form.Label>
-							<Form.Control type="text" id="name" name="name" />
-						</Form.Group>
-						<Form.Group>
-							<Form.Label htmlFor="imageUrl">Image URL: </Form.Label>
-							<Form.Control type="text" id="imageUrl" name="imageUrl" />
-						</Form.Group>
+  return (
+    <div className="Create">
+      <Container fluid="true">
+        <Form onSubmit={postNewPortfolio}>
+          <Form.Row>
+            <Form.Group>
+              <Form.Label htmlFor="name">User name: </Form.Label>
+              <Form.Control type="text" id="name" name="name" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="imageUrl">Image URL: </Form.Label>
+              <Form.Control type="text" id="imageUrl" name="imageUrl" />
+            </Form.Group>
 
-						<Form.Group>
-							<Form.Label htmlFor="title">Title: </Form.Label>
-							<Form.Control type="text" id="title" name="title" />
-						</Form.Group>
-					</Form.Row>
-					<Form.Row>
-						<Form.Group>
-							<Form.Label htmlFor="link">Portfolio link: </Form.Label>
-							<Form.Control type="text" id="link" name="link" />
-						</Form.Group>
-						<Form.Group>
-							<Form.Label htmlFor="description">Description: </Form.Label>
-							<Form.Control type="text" id="description" name="description" />
-							<Button type="submit">Submit</Button>
-						</Form.Group>
-					</Form.Row>
-				</Form>
-			</Container>
-		</div>
-	);
+            <Form.Group>
+              <Form.Label htmlFor="title">Title: </Form.Label>
+              <Form.Control type="text" id="title" name="title" />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group>
+              <Form.Label htmlFor="link">Portfolio link: </Form.Label>
+              <Form.Control type="text" id="link" name="link" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="description">Description: </Form.Label>
+              <Form.Control type="text" id="description" name="description" />
+              <Button className="btn btn-secondary" type="submit">
+                Submit
+              </Button>
+            </Form.Group>
+          </Form.Row>
+        </Form>
+      </Container>
+    </div>
+  );
 }
 
 export default Create;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,73 +2,78 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class Portfolio extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			port: null
-		};
-	}
-	deletePortfolio = e => {
-		console.log(e.target.id);
-		fetch(
-			`https://portfolio-rater.herokuapp.com/api/portfolios/delete/${e.target.id}`,
-			{
-				method: 'DELETE',
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			}
-		).then(response =>
-			response.json().then(json => {
-				console.log(json);
-			})
-		);
-	};
-	componentDidMount() {
-		fetch(
-			'https://portfolio-rater.herokuapp.com/api/portfolios/' +
-				this.props.match.params.id
-		)
-			.then(response => response.json())
-			.then(data => this.setState({ port: data }))
-			.catch(console.error);
-	}
-	render() {
-		console.log(this.state);
-		if (this.state.port !== null) {
-			const { port } = this.state;
-			let comments = port.posts.map(comment => {
-				return <li key={comment}>{comment}</li>;
-			});
-			return (
-				<div key={port._id}>
-					<Link to={'/portfolio/' + port._id}>
-						<img src={port.imageUrl} />
-					</Link>
-					<br />
-					<h4>{port.name}</h4>
-					<h3>{port.title}</h3>
-					<p>{port.description}</p>
-					<ul>{comments}</ul>
-					<Button href={port.link}>Visit portfolio</Button>
-					{true && (
-						<div>
-							<Link to={'/update/' + port._id}>
-								<Button id={port._id}>Edit</Button>
-							</Link>
-							<Button id={port._id} onClick={this.deletePortfolio}>
-								Delete
-							</Button>
-						</div>
-					)}
-				</div>
-			);
-		} else {
-			return <h1>Portfolio not found?</h1>;
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      port: null
+    };
+  }
+  deletePortfolio = e => {
+    console.log(e.target.id);
+    fetch(
+      `https://portfolio-rater.herokuapp.com/api/portfolios/delete/${e.target.id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(response =>
+      response.json().then(json => {
+        console.log(json);
+      })
+    );
+  };
+  componentDidMount() {
+    fetch(
+      'https://portfolio-rater.herokuapp.com/api/portfolios/' +
+        this.props.match.params.id
+    )
+      .then(response => response.json())
+      .then(data => this.setState({ port: data }))
+      .catch(console.error);
+  }
+  render() {
+    console.log(this.state);
+    if (this.state.port !== null) {
+      const { port } = this.state;
+      let comments = port.posts.map(comment => {
+        return <li key={comment}>{comment}</li>;
+      });
+      return (
+        <div key={port._id}>
+          <Link to={'/portfolio/' + port._id}>
+            <img src={port.imageUrl} />
+          </Link>
+          <br />
+          <h4>{port.name}</h4>
+          <h3>{port.title}</h3>
+          <p>{port.description}</p>
+          <ul>{comments}</ul>
+
+          <Button className="btn btn-secondary" href={port.link}>
+            Visit portfolio
+          </Button>
+
+          {true && (
+            <div>
+              <Link to={'/update/' + port._id}>
+                <Button id={port._id}>Edit</Button>
+              </Link>
+              <Button id={port._id} onClick={this.deletePortfolio}>
+                Delete
+              </Button>
+            </div>
+          )}
+        </div>
+      );
+    } else {
+      return <h1>Portfolio not found?</h1>;
+    }
+  }
 }
 
 export default Portfolio;
