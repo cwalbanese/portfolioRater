0078e1c5f5cea6eb16e59548449006c7190eddf5
SRBond <skyler.bond7@gmail.com>
2020-02-18 10:12:25 -0800

    fix conflict

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import './Home.css';
-
 import Button from 'react-bootstrap/Button';
 // import Col from "react-bootstrap/Col";
 // import Row from "react-bootstrap/Row";
