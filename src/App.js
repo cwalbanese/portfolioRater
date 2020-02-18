fa9296bdea91d8dcfdadf6bee99377bfa89b414b
cwalbanese <631gti@gmail.com>
2020-02-18 12:36:47 -0500

    Updated css
    Update css for home and app components

--- src/App.js
+++ src/App.js
@@ -1,91 +1,87 @@
-import React from "react";
-import "./App.css";
-import { Link, Route, Switch } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
 
-import Create from "./components/Create/Create";
-import About from "./components/About/About";
-import Home from "./components/Home/Home";
-import Update from "./components/Update/Update";
+import Create from './components/Create/Create';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Update from './components/Update/Update';
 
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentUser: {},
-			selectedPortfolio: {},
-			portfolios: []
-		};
-	}
-	updateCurrentUser = user => {
-		this.setState({
-			currentUser: user
-		});
-	};
-	componentDidMount() {
-		//fetch all portfolios to apss to Home
-		fetch("https://portfolio-rater.herokuapp.com/api/portfolios")
-			.then(response => response.json())
-			.then(data => this.setState({ portfolios: data }))
-			.catch(console.error);
-	}
-	render() {
-		return (
-			<div className="App">
-				<nav>
-					<Container>
-						<Row>
-							<Col>
-								<Button className="btn btn-primary">
-									<Link className="homeLink" to="/">
-										Home
-									</Link>
-								</Button>
-							</Col>
-							<Col>
-								<Button className="btn btn-secondary">
-									<Link className="createLink" to="/create">
-										Create
-									</Link>
-								</Button>
-							</Col>
-							<Col>
-								<Button className="btn btn-secondary">
-									<Link className="aboutLink" to="/about">
-										About
-									</Link>
-								</Button>
-							</Col>
-						</Row>
-					</Container>
-				</nav>
-				<main>
-					<Switch>
-						<Route path="/create" render={() => <Create />} />
-						<Route path="/about" render={() => <About />} />
-						<Route
-							path="/update/:id"
-							render={props => <Update match={props.match} />}
-						/>
-						<Route
-							exact
-							path="/"
-							render={() => (
-								<Home
-									ports={this.state.portfolios}
-									user={this.state.currentUser}
-								/>
-							)}
-						/>
-					</Switch>
-				</main>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {},
+      selectedPortfolio: {},
+      portfolios: []
+    };
+  }
+  updateCurrentUser = user => {
+    this.setState({
+      currentUser: user
+    });
+  };
+  componentDidMount() {
+    //fetch all portfolios to apss to Home
+    fetch('https://portfolio-rater.herokuapp.com/api/portfolios')
+      .then(response => response.json())
+      .then(data => this.setState({ portfolios: data }))
+      .catch(console.error);
+  }
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <h2>Portfolio Rater</h2>
+          <Button className="btn btn-primary">
+            <Link className="homeLink" to="/">
+              Home
+            </Link>
+          </Button>
+
+          <Button className="btn btn-secondary">
+            <Link className="createLink" to="/create">
+              Create
+            </Link>
+          </Button>
+
+          <Button className="btn btn-secondary">
+            <Link className="aboutLink" to="/about">
+              About
+            </Link>
+          </Button>
+        </nav>
+        <main>
+          <Switch>
+            <Route path="/create" render={() => <Create />} />
+            <Route path="/about" render={() => <About />} />
+            <Route
+              path="/update/:id"
+              render={props => <Update match={props.match} />}
+            />
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <Home
+                  ports={this.state.portfolios}
+                  user={this.state.currentUser}
+                />
+              )}
+            />
+          </Switch>
+        </main>
+        <footer>
+          <p>Made by Chris Albanese, Skyler Bond, and Jarod McGill</p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,63 +1,63 @@
-import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
-
-import Button from "react-bootstrap/Button";
+import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import './Home.css';
+import Button from 'react-bootstrap/Button';
 // import Col from "react-bootstrap/Col";
 // import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+import Container from 'react-bootstrap/Container';
 
 function Home(props) {
-	const { ports, user } = props;
+  const { ports, user } = props;
 
-	function deletePortfolio(e) {
-		console.log(e.target.id);
-		fetch(
-			`https://portfolio-rater.herokuapp.com/api/portfolios/delete/${e.target.id}`,
-			{
-				method: "DELETE",
-				headers: {
-					"Content-Type": "application/json"
-				}
-			}
-		).then(response =>
-			response.json().then(json => {
-				console.log(json);
-			})
-		);
-	}
+  function deletePortfolio(e) {
+    console.log(e.target.id);
+    fetch(
+      `https://portfolio-rater.herokuapp.com/api/portfolios/delete/${e.target.id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(response =>
+      response.json().then(json => {
+        console.log(json);
+      })
+    );
+  }
 
-	let portElems = ports.map(port => {
-		let comments = port.posts.map(comment => {
-			return <li key={comment}>{comment}</li>;
-		});
-		return (
-			<div key={port._id}>
-				<img src={port.imageUrl} />
-				<br />
-				<h4>{port.name}</h4>
-				<h3>{port.title}</h3>
-				<p>{port.description}</p>
-				<ul>{comments}</ul>
-				<Button href={port.link}>Visit portfolio</Button>
-				{true && (
-					<div>
-						<Link to={"/update/" + port._id}>
-							<Button id={port._id}>Edit</Button>
-						</Link>
-						<Button id={port._id} onClick={deletePortfolio}>
-							Delete
-						</Button>
-					</div>
-				)}
-			</div>
-		);
-	});
+  let portElems = ports.map(port => {
+    let comments = port.posts.map(comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+    return (
+      <div key={port._id} className="portfolio">
+        <img src={port.imageUrl} alt="" />
+        <br />
+        <h3>{port.name}</h3>
+        <h4>{port.title}</h4>
+        <p>{port.description}</p>
+        <ul>{comments}</ul>
+        <Button href={port.link}>Visit portfolio</Button>
+        {true && (
+          <div>
+            <Link to={'/update/' + port._id}>
+              <Button id={port._id}>Edit</Button>
+            </Link>
+            <Button id={port._id} onClick={deletePortfolio}>
+              Delete
+            </Button>
+          </div>
+        )}
+      </div>
+    );
+  });
 
-	if (portElems) {
-		return <Container className="Home">{portElems}</Container>;
-	} else {
-		return <h1>No portfolios found?</h1>;
-	}
+  if (portElems) {
+    return <div className="main">{portElems}</div>;
+  } else {
+    return <h1>No portfolios found?</h1>;
+  }
 }
 
 export default Home;
