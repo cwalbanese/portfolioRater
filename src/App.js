c754d662b1c337b2ee5806415422728b5dfe12d6
SRBond <skyler.bond7@gmail.com>
2020-02-18 10:05:43 -0800

    fix conflict

--- src/App.js
+++ src/App.js
@@ -14,76 +14,77 @@ import Container from 'react-bootstrap/Container';
 
 class App extends React.Component {
 	constructor(props) {
-		super(props);
-		this.state = {
-			currentUser: {},
-			selectedPortfolio: {},
-			portfolios: []
-		};
-	}
-	updateCurrentUser = user => {
-		this.setState({
-			currentUser: user
-		});
-	};
-	componentDidMount() {
-		//fetch all portfolios to apss to Home
-		fetch('https://portfolio-rater.herokuapp.com/api/portfolios')
-			.then(response => response.json())
-			.then(data => this.setState({ portfolios: data }))
-			.catch(console.error);
-	}
-	render() {
-		return (
-			<div className="App">
-				<nav>
-					<h2>Portfolio Rater</h2>
-					<Button className="btn btn-primary">
-						<Link className="homeLink" to="/">
-							Home
-						</Link>
-					</Button>
-					<Button className="btn btn-secondary">
-						<Link className="createLink" to="/create">
-							Create
-						</Link>
-					</Button>
-					<Button className="btn btn-secondary">
-						<Link className="aboutLink" to="/about">
-							About
-						</Link>
-					</Button>
-				</nav>
-				<main>
-					<Switch>
-						<Route path="/create" render={() => <Create />} />
-						<Route path="/about" render={() => <About />} />
-						<Route
-							path="/update/:id"
-							render={props => <Update match={props.match} />}
-						/>
-						<Route
-							path="/portfolio/:id"
-							render={props => <Portfolio match={props.match} />}
-						/>
-						<Route
-							exact
-							path="/"
-							render={() => (
-								<Home
-									ports={this.state.portfolios}
-									user={this.state.currentUser}
-								/>
-							)}
-						/>
-					</Switch>
-				</main>
-				<footer>
-					<p>Made by Chris Albanese, Skyler Bond, and Jarod McGill</p>
-				</footer>
-			</div>
-		);
-	}
-}
+    super(props);
+    this.state = {
+      currentUser: {},
+      selectedPortfolio: {},
+      portfolios: []
+    };
+  }
+  updateCurrentUser = user => {
+    this.setState({
+      currentUser: user
+    });
+  };
+  componentDidMount() {
+    //fetch all portfolios to apss to Home
+    fetch('https://portfolio-rater.herokuapp.com/api/portfolios')
+      .then(response => response.json())
+      .then(data => this.setState({ portfolios: data }))
+      .catch(console.error);
+  }
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <h2>Portfolio Rater</h2>
+          <Button className="btn btn-primary">
+            <Link className="homeLink" to="/">
+              Home
+            </Link>
+          </Button>
+
+          <Button className="btn btn-secondary">
+            <Link className="createLink" to="/create">
+              Create
+            </Link>
+          </Button>
+
+          <Button className="btn btn-secondary">
+            <Link className="aboutLink" to="/about">
+              About
+            </Link>
+          </Button>
+        </nav>
+        <main>
+          <Switch>
+            <Route path="/create" render={() => <Create />} />
+            <Route path="/about" render={() => <About />} />
+            <Route
+              path="/update/:id"
+              render={props => <Update match={props.match} />}
+            />
+            <Route
+              path="/portfolio/:id"
+              render={props => <Portfolio match={props.match} />}
+            />
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <Home
+                  ports={this.state.portfolios}
+                  user={this.state.currentUser}
+                />
+              )}
+            />
+          </Switch>
+        </main>
+        <footer>
+          <p>Made by Chris Albanese, Skyler Bond, and Jarod McGill</p>
+        </footer>
+      </div>
+    );
+  }
 
 export default App;
