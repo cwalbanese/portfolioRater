f5d252f1ba4fc862898864cbc383a15cea763303
SRBond <skyler.bond7@gmail.com>
2020-02-19 07:50:11 -0800

    Add view/comment button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -72,6 +72,7 @@ function Home(props) {
 					<Link to={'/portfolio/' + port._id}>
 						<Button>View/Comment</Button>
 					</Link>
+					{/* test */}
 					<Button
 						onClick={() => like(port._id, port.rating)}
 						className="btn btn-success"
