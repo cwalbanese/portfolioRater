c1560e019d45e3a490f2fd06b7fc24b0fdd846cc
cwalbanese <631gti@gmail.com>
2020-05-07 11:48:53 -0400

    updated css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,60 +7,60 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Login(props) {
-	return (
-		<Container className="login">
-			<Row>
-				<Col></Col>
-				<Col>
-					<Modal.Dialog className="modal-dialog-centered modal-fade">
-						<Modal.Header>
-							<Modal.Title>Please enter your user name</Modal.Title>
-						</Modal.Header>
+  return (
+    <Container className="login">
+      <Row>
+        <Col></Col>
+        <Col>
+          <Modal.Dialog className="modal-dialog-centered modal-fade">
+            <Modal.Header>
+              <Modal.Title>Please enter your user name or sign-up</Modal.Title>
+            </Modal.Header>
 
-						<Modal.Body>
-							<Form onSubmit={props.handleLogin}>
-								<Form.Group>
-									<Form.Label id="username" name="username">
-										User name:
-									</Form.Label>
-									<Form.Control
-										className="loginInput"
-										type="text"
-										htmlFor="username"
-										id="username"
-										name="username"
-									/>
-									<Button
-										className="cancelBtn"
-										onClick={props.handleHide}
-										variant="secondary"
-									>
-										Cancel
-									</Button>
-									<Button
-										onClick={() => {
-											setTimeout(() => {
-												props.handleHide();
-											}, 250);
-										}}
-										id={props.id}
-										type="submit"
-										variant="primary"
-										className="loginBtn"
-									>
-										Login
-									</Button>
-								</Form.Group>
-							</Form>
-						</Modal.Body>
+            <Modal.Body>
+              <Form onSubmit={props.handleLogin}>
+                <Form.Group>
+                  <Form.Label id="username" name="username">
+                    User name:
+                  </Form.Label>
+                  <Form.Control
+                    className="loginInput"
+                    type="text"
+                    htmlFor="username"
+                    id="username"
+                    name="username"
+                  />
+                  <Button
+                    className="cancelBtn"
+                    onClick={props.handleHide}
+                    variant="secondary"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setTimeout(() => {
+                        props.handleHide();
+                      }, 250);
+                    }}
+                    id={props.id}
+                    type="submit"
+                    variant="primary"
+                    className="loginBtn"
+                  >
+                    Login
+                  </Button>
+                </Form.Group>
+              </Form>
+            </Modal.Body>
 
-						<Modal.Footer></Modal.Footer>
-					</Modal.Dialog>
-				</Col>
-				<Col></Col>
-			</Row>
-		</Container>
-	);
+            <Modal.Footer></Modal.Footer>
+          </Modal.Dialog>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Login;
